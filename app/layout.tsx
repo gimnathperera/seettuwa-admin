f5dc6c81b62532c4900d5fcbb2ee49f46e78216d
Gimnath-Perera <gimnathperera@gmail.com>
2023-08-06 13:08:56 +0530

    SO-24: providers separated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import { Metadata } from 'next';
 import clsx from 'clsx';
 import { siteConfig } from '@/config/site';
 import { fontSans } from '@/config/fonts';
-import { Navbar } from '@/components/navbar';
-import { Link } from '@nextui-org/link';
-import '@/styles/globals.css';
+import { Navbar } from '@/components/shared/header';
+import Sidebar from '@/components/shared/sidebar';
 import { ConvexClientProvider } from '@/providers/ConvexClientProvider';
 import { NextUIThemeProvider } from '@/providers//ThemeProvider';
+import '@/styles/globals.css';
+import Footer from '@/components/shared/footer';
 
 export const metadata: Metadata = {
   title: {
@@ -31,23 +32,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
       <head />
       <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
         <NextUIThemeProvider themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-          <div className='relative flex flex-col h-screen'>
-            <Navbar />
-            <main className='container mx-auto max-w-7xl pt-16 px-6 flex-grow'>
-              <ConvexClientProvider>{children}</ConvexClientProvider>
-            </main>
-            <footer className='w-full flex items-center justify-center py-3'>
-              <Link
-                isExternal
-                className='flex items-center gap-1 text-current'
-                href='https://nextui-docs-v2.vercel.app?utm_source=next-app-template'
-                title='nextui.org homepage'
-              >
-                <span className='text-default-600'>Powered by</span>
-                <p className='text-primary'>NextUI</p>
-              </Link>
-            </footer>
-          </div>
+          <ConvexClientProvider>
+            <div className='relative flex flex-col h-screen'>
+              <Navbar />
+              <Sidebar />
+              <main className='mx-auto max-w-7xl pt-16 flex-grow'>{children}</main>
+              <Footer />
+            </div>
+          </ConvexClientProvider>
         </NextUIThemeProvider>
       </body>
     </html>

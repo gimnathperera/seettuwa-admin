aaa7ab5c9fb91e1ce45f4f63bc9edb469df00ac9
Gimnath-Perera <gimnathperera@gmail.com>
2023-08-07 15:46:43 +0530

    SO-25: convex re-initialize

--- convex/_generated/dataModel.d.ts
+++ convex/_generated/dataModel.d.ts
@@ -9,8 +9,8 @@
  * @module
  */
 
-import { AnyDataModel } from "convex/server";
-import type { GenericId } from "convex/values";
+import { AnyDataModel } from 'convex/server';
+import type { GenericId } from 'convex/values';
 
 /**
  * No `schema.ts` file found!
@@ -44,8 +44,7 @@ export type Doc = any;
  * IDs are just strings at runtime, but this type can be used to distinguish them from other
  * strings when type checking.
  */
-export type Id<TableName extends TableNames = TableNames> =
-  GenericId<TableName>;
+export type Id<TableName extends TableNames = TableNames> = GenericId<TableName>;
 
 /**
  * A type describing your Convex data model.

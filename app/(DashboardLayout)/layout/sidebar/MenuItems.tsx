9fa193ff117f47926e3c2a4d44bb9983945f7b33
Gimnath-Perera <gimnathperera@gmail.com>
2023-08-12 08:14:46 +0530

    SO-25: cell forucs handled

--- app/(DashboardLayout)/layout/sidebar/MenuItems.tsx
+++ app/(DashboardLayout)/layout/sidebar/MenuItems.tsx
@@ -1,9 +1,7 @@
 import {
-  IconAperture,
   IconCopy,
   IconLayoutDashboard,
   IconLogin,
-  IconMoodHappy,
   IconTypography,
   IconUserPlus,
 } from '@tabler/icons-react';
@@ -64,22 +62,6 @@ const Menuitems = [
     title: 'Register',
     icon: IconUserPlus,
     href: '/authentication/register',
-  },
-  {
-    navlabel: true,
-    subheader: 'Extra',
-  },
-  {
-    id: uniqueId(),
-    title: 'Icons',
-    icon: IconMoodHappy,
-    href: '/icons',
-  },
-  {
-    id: uniqueId(),
-    title: 'Sample Page',
-    icon: IconAperture,
-    href: '/sample-page',
   },
 ];
 

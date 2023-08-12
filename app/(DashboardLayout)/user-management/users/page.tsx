33e744889da60379927dd4fb91cccb16fc686f16
Gimnath-Perera <gimnathperera@gmail.com>
2023-08-12 18:59:59 +0530

    SO-29: component import structure added

--- app/(DashboardLayout)/user-management/users/page.tsx
+++ app/(DashboardLayout)/user-management/users/page.tsx
@@ -38,6 +38,7 @@ const columns: GridColDef[] = [
     field: 'actions',
     headerName: 'Actions',
     renderCell: (): JSX.Element => {
+      // TODO: add tooltips for the actions
       return (
         <Box flexDirection='row'>
           <IconButton size='small'>

347e0d6fa86b88696759c8a72ee3c686582c672b
Gimnath-Perera <gimnathperera@gmail.com>
2023-09-20 05:39:13 +0530

    feat: add avatar icons in user table

--- app/(dashboard)/user-management/users/page.tsx
+++ app/(dashboard)/user-management/users/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { Box, Button, Grid, IconButton, Typography } from '@mui/material';
-import { GridCellParams, GridColDef, GridValueGetterParams, useGridApiRef } from '@mui/x-data-grid';
+import { GridCellParams, GridColDef, useGridApiRef } from '@mui/x-data-grid';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import { useRouter } from 'next/navigation';
 import { TableOuterCard, PageContainer, DataTable, TableFilterBar } from '@/components';
+import Link from 'next/link';
+import LetterAvatar from '@/components/shared/avatar';
 
 const rows = [
   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
@@ -24,30 +26,36 @@ const Users: React.FC = () => {
   const gridApiRef = useGridApiRef();
   const router = useRouter();
 
+  const renderClient = (fullName: string): JSX.Element => {
+    return <LetterAvatar fullName={fullName} />;
+  };
+
   const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', flex: 1 },
     {
-      field: 'firstName',
-      headerName: 'First name',
+      field: 'fullName',
+      headerName: 'Full name',
       flex: 1,
+      renderCell: ({ row }: GridCellParams): JSX.Element => {
+        const { firstName, lastName, id } = row;
+
+        return (
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            {renderClient(`${firstName} ${lastName}`)}
+            <Box sx={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column' }}>
+              <Link href={`/user-management/users/${id}`}>{`${firstName} ${lastName}`}</Link>
+            </Box>
+          </Box>
+        );
+      },
     },
-    {
-      field: 'lastName',
-      headerName: 'Last name',
-      flex: 1,
-    },
+
     {
       field: 'age',
       headerName: 'Age',
       flex: 1,
     },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      flex: 1,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
+
     {
       flex: 1,
       field: 'actions',
